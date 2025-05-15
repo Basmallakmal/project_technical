3caf8e3b5da02626be65c46298862c4b396f8de1
Akmall <akmalfaseh18@gmail.com>
2025-05-15 13:18:52 +0700

    Refactor networking.dart to uncomment baseUrl and baseUrlImage assignments for improved API configuration

--- lib/presentation/pages/dashboard/provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'state.dart';
-
-class DashboardProvider extends ChangeNotifier {
-  final state = DashboardState();
-}
--- lib/presentation/pages/login/provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'state.dart';
-
-class LoginProvider extends ChangeNotifier {
-  final state = LoginState();
-}
